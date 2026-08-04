64bcfb67d9a8aede0250633eecd68572971977a3
iggy <iggyzhao0222@gmail.com>
2026-08-04 16:39:59 -0400

    Move Outstanding Reviewer Award to top of research honors

--- src/data.js
+++ src/data.js
@@ -236,10 +236,10 @@ window.CV = {
 
   honors: {
     researchHonors: [
+      "Outstanding Reviewer Award, AOM CTO Division (2026)",
       "Outstanding Editor, Information Technology for Development, Awarded by AIS (2026)",
       "Junior Faculty Consortium Fellow, AOM CTO Division (2026)",
       "AIS Distinguished Member (2026)",
-      "Outstanding Reviewer Award, AOM CTO Division (2026)",
       "Best ERF Paper Award, AMCIS (2025)",
       "MIS Quarterly Author Development Workshop Selected Attendee (2024)",
       "MIS Quarterly Reviewer Development Workshop Selected Attendee (2024, 2025)",
